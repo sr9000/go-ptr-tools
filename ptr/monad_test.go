0109c31929b0f1cf00382bf5b6add66d3351ea65
sr9000 <Pirat-spy@yandex.ru>
2025-07-05 22:24:11 +0300

    ft-optional-value: monad done

--- ptr/monad_test.go
+++ ptr/monad_test.go
@@ -82,8 +82,8 @@ func TestApply(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 
-			result := ptr.Apply(tt.input, add1)
-			require.Equal(t, tt.expected, result)
+			res := ptr.Apply(tt.input, add1)
+			require.Equal(t, tt.expected, res)
 		})
 	}
 }
@@ -126,8 +126,8 @@ func TestApplyCtx(t *testing.T) {
 				ctx = cancelledCtx(t)
 			}
 
-			result := ptr.ApplyCtx(ctx, tt.input, addCtx)
-			require.Equal(t, tt.expected, result)
+			res := ptr.ApplyCtx(ctx, tt.input, addCtx)
+			require.Equal(t, tt.expected, res)
 		})
 	}
 }
@@ -165,12 +165,12 @@ func TestApplyErr(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 
-			result, err := ptr.ApplyErr(tt.input, addErr)
+			res, err := ptr.ApplyErr(tt.input, addErr)
 			if tt.expectError != nil {
 				require.ErrorIs(t, err, tt.expectError)
 			} else {
 				require.NoError(t, err)
-				require.Equal(t, tt.expected, result)
+				require.Equal(t, tt.expected, res)
 			}
 		})
 	}
@@ -225,13 +225,13 @@ func TestApplyCtxErr(t *testing.T) {
 				ctx = cancelledCtx(t)
 			}
 
-			result, err := ptr.ApplyCtxErr(ctx, tt.input, addCtxErr)
+			res, err := ptr.ApplyCtxErr(ctx, tt.input, addCtxErr)
 
 			if tt.expectError != nil {
 				require.ErrorIs(t, err, tt.expectError)
 			} else {
 				require.NoError(t, err)
-				require.Equal(t, tt.expected, result)
+				require.Equal(t, tt.expected, res)
 			}
 		})
 	}
@@ -789,16 +789,15 @@ func TestApply9(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 
-			result := ptr.Apply9(
+			res := ptr.Apply9(
 				tt.inputs[0], tt.inputs[1], tt.inputs[2],
 				tt.inputs[3], tt.inputs[4], tt.inputs[5],
 				tt.inputs[6], tt.inputs[7], tt.inputs[8],
 				func(num1, num2, num3, num4, num5, num6, num7, num8, num9 int) int {
 					return num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9
-				},
-			)
+				})
 
-			require.Equal(t, tt.expected, result)
+			require.Equal(t, tt.expected, res)
 		})
 	}
 }
@@ -847,7 +846,7 @@ func TestApply9Ctx(t *testing.T) {
 				ctx = cancelledCtx(t)
 			}
 
-			result := ptr.Apply9Ctx(
+			res := ptr.Apply9Ctx(
 				ctx,
 				tt.inputs[0], tt.inputs[1], tt.inputs[2],
 				tt.inputs[3], tt.inputs[4], tt.inputs[5],
@@ -860,10 +859,9 @@ func TestApply9Ctx(t *testing.T) {
 					}
 
 					return num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9
-				},
-			)
+				})
 
-			require.Equal(t, tt.expected, result)
+			require.Equal(t, tt.expected, res)
 		})
 	}
 }
@@ -907,7 +905,7 @@ func TestApply9Err(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 
-			result, err := ptr.Apply9Err(
+			res, err := ptr.Apply9Err(
 				tt.inputs[0], tt.inputs[1], tt.inputs[2],
 				tt.inputs[3], tt.inputs[4], tt.inputs[5],
 				tt.inputs[6], tt.inputs[7], tt.inputs[8],
@@ -917,14 +915,13 @@ func TestApply9Err(t *testing.T) {
 					}
 
 					return num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9, nil
-				},
-			)
+				})
 
 			if tt.expectError != nil {
 				require.ErrorIs(t, err, tt.expectError)
 			} else {
 				require.NoError(t, err)
-				require.Equal(t, tt.expected, result)
+				require.Equal(t, tt.expected, res)
 			}
 		})
 	}
@@ -986,7 +983,7 @@ func TestApply9CtxErr(t *testing.T) {
 				ctx = cancelledCtx(t)
 			}
 
-			result, err := ptr.Apply9CtxErr(
+			res, err := ptr.Apply9CtxErr(
 				ctx,
 				tt.inputs[0], tt.inputs[1], tt.inputs[2],
 				tt.inputs[3], tt.inputs[4], tt.inputs[5],
@@ -1003,14 +1000,363 @@ func TestApply9CtxErr(t *testing.T) {
 					}
 
 					return num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9, nil
-				},
-			)
+				})
 
 			if tt.expectError != nil {
 				require.ErrorIs(t, err, tt.expectError)
 			} else {
 				require.NoError(t, err)
-				require.Equal(t, tt.expected, result)
+				require.Equal(t, tt.expected, res)
+			}
+		})
+	}
+}
+
+type result struct {
+	sum, product, min, max, avg int
+}
+
+func requireAllNil(t *testing.T, sum *int, product *int, mn *int, mx *int, avg *int) {
+	t.Helper()
+
+	require.Nil(t, sum)
+	require.Nil(t, product)
+	require.Nil(t, mn)
+	require.Nil(t, mx)
+	require.Nil(t, avg)
+}
+
+func requireExpected(t *testing.T, expected result, sum *int, product *int, mn *int, mx *int, avg *int) {
+	t.Helper()
+
+	require.Equal(t, expected.sum, *sum)
+	require.Equal(t, expected.product, *product)
+	require.Equal(t, expected.min, *mn)
+	require.Equal(t, expected.max, *mx)
+	require.Equal(t, expected.avg, *avg)
+}
+
+func TestApply95(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name     string
+		inputs   []*int
+		expected *result
+	}{
+		{
+			name:   "normal case",
+			inputs: ptrsArray([]int{1, 2, 3, 4, 5, 6, 7, 8, 9}),
+			expected: &result{
+				sum:     45,
+				product: 362880,
+				min:     1,
+				max:     9,
+				avg:     5,
+			},
+		},
+		{
+			name:     "one nil input",
+			inputs:   ptrsArray([]int{1, 0, 3, 4, 5, 6, 7, 8, 9}),
+			expected: nil,
+		},
+		{
+			name:     "all nil inputs",
+			inputs:   ptrsArray([]int{0, 0, 0, 0, 0, 0, 0, 0, 0}),
+			expected: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			sum, product, minimum, maximum, avg := ptr.Apply95(
+				tt.inputs[0], tt.inputs[1], tt.inputs[2],
+				tt.inputs[3], tt.inputs[4], tt.inputs[5],
+				tt.inputs[6], tt.inputs[7], tt.inputs[8],
+				func(num1, num2, num3, num4, num5, num6, num7, num8, num9 int) (sum, product, mn, mx, avg int) {
+					sum = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9
+					product = num1 * num2 * num3 * num4 * num5 * num6 * num7 * num8 * num9
+					mn = min(num1, num2, num3, num4, num5, num6, num7, num8, num9)
+					mx = max(num1, num2, num3, num4, num5, num6, num7, num8, num9)
+					avg = sum / 9
+
+					return
+				})
+
+			if tt.expected == nil {
+				requireAllNil(t, sum, product, minimum, maximum, avg)
+			} else {
+				requireExpected(t, *tt.expected, sum, product, minimum, maximum, avg)
+			}
+		})
+	}
+}
+
+func TestApply95Ctx(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name         string
+		ctxCancelled bool
+		inputs       []*int
+		expected     *result
+	}{
+		{
+			name:         "normal case",
+			ctxCancelled: false,
+			inputs:       ptrsArray([]int{1, 2, 3, 4, 5, 6, 7, 8, 9}),
+			expected: &result{
+				sum:     45,
+				product: 362880,
+				min:     1,
+				max:     9,
+				avg:     5,
+			},
+		},
+		{
+			name:         "cancelled context",
+			ctxCancelled: true,
+			inputs:       ptrsArray([]int{1, 2, 3, 4, 5, 6, 7, 8, 9}),
+			expected: &result{
+				sum:     0,
+				product: 0,
+				min:     0,
+				max:     0,
+				avg:     0,
+			},
+		},
+		{
+			name:         "one nil input",
+			ctxCancelled: false,
+			inputs:       ptrsArray([]int{1, 0, 3, 4, 5, 6, 7, 8, 9}),
+			expected:     nil,
+		},
+		{
+			name:         "all nil inputs",
+			ctxCancelled: false,
+			inputs:       ptrsArray([]int{0, 0, 0, 0, 0, 0, 0, 0, 0}),
+			expected:     nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			ctx := t.Context()
+			if tt.ctxCancelled {
+				ctx = cancelledCtx(t)
+			}
+
+			sum, product, minimum, maximum, avg := ptr.Apply95Ctx(
+				ctx,
+				tt.inputs[0], tt.inputs[1], tt.inputs[2],
+				tt.inputs[3], tt.inputs[4], tt.inputs[5],
+				tt.inputs[6], tt.inputs[7], tt.inputs[8],
+				func(ctx context.Context, num1, num2, num3, num4, num5, num6, num7, num8, num9 int,
+				) (sum, product, mn, mx, avg int) {
+					select {
+					default:
+					case <-ctx.Done():
+						return 0, 0, 0, 0, 0
+					}
+
+					sum = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9
+					product = num1 * num2 * num3 * num4 * num5 * num6 * num7 * num8 * num9
+					mn = min(num1, num2, num3, num4, num5, num6, num7, num8, num9)
+					mx = max(num1, num2, num3, num4, num5, num6, num7, num8, num9)
+					avg = sum / 9
+
+					return
+				})
+
+			if tt.expected == nil {
+				requireAllNil(t, sum, product, minimum, maximum, avg)
+			} else {
+				requireExpected(t, *tt.expected, sum, product, minimum, maximum, avg)
+			}
+		})
+	}
+}
+
+func TestApply95Err(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name        string
+		inputs      []*int
+		expected    *result
+		expectError error
+	}{
+		{
+			name:   "normal case",
+			inputs: ptrsArray([]int{1, 2, 3, 4, 5, 6, 7, 8, 9}),
+			expected: &result{
+				sum:     45,
+				product: 362880,
+				min:     1,
+				max:     9,
+				avg:     5,
+			},
+			expectError: nil,
+		},
+		{
+			name:        "error case",
+			inputs:      ptrsArray([]int{1, 2, 3, 4, -5, 6, 7, 8, 9}),
+			expected:    nil,
+			expectError: errNegativeNumber,
+		},
+		{
+			name:        "one nil input",
+			inputs:      ptrsArray([]int{1, 0, 3, 4, 5, 6, 7, 8, 9}),
+			expected:    nil,
+			expectError: nil,
+		},
+		{
+			name:        "all nil inputs",
+			inputs:      ptrsArray([]int{0, 0, 0, 0, 0, 0, 0, 0, 0}),
+			expected:    nil,
+			expectError: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			sum, product, minimum, maximum, avg, err := ptr.Apply95Err(
+				tt.inputs[0], tt.inputs[1], tt.inputs[2],
+				tt.inputs[3], tt.inputs[4], tt.inputs[5],
+				tt.inputs[6], tt.inputs[7], tt.inputs[8],
+				func(num1, num2, num3, num4, num5, num6, num7, num8, num9 int) (sum, product, mn, mx, avg int, err error) {
+					if min(num1, num2, num3, num4, num5, num6, num7, num8, num9) < 0 {
+						return 0, 0, 0, 0, 0, errNegativeNumber
+					}
+
+					sum = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9
+					product = num1 * num2 * num3 * num4 * num5 * num6 * num7 * num8 * num9
+					mn = min(num1, num2, num3, num4, num5, num6, num7, num8, num9)
+					mx = max(num1, num2, num3, num4, num5, num6, num7, num8, num9)
+					avg = sum / 9
+
+					return
+				})
+
+			if tt.expectError != nil {
+				require.ErrorIs(t, err, tt.expectError)
+			} else {
+				require.NoError(t, err)
+
+				if tt.expected == nil {
+					requireAllNil(t, sum, product, minimum, maximum, avg)
+				} else {
+					requireExpected(t, *tt.expected, sum, product, minimum, maximum, avg)
+				}
+			}
+		})
+	}
+}
+
+func TestApply95CtxErr(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name         string
+		ctxCancelled bool
+		inputs       []*int
+		expected     *result
+		expectError  error
+	}{
+		{
+			name:         "normal case",
+			ctxCancelled: false,
+			inputs:       ptrsArray([]int{1, 2, 3, 4, 5, 6, 7, 8, 9}),
+			expected: &result{
+				sum:     45,
+				product: 362880,
+				min:     1,
+				max:     9,
+				avg:     5,
+			},
+			expectError: nil,
+		},
+		{
+			name:         "cancelled context",
+			ctxCancelled: true,
+			inputs:       ptrsArray([]int{1, 2, 3, 4, 5, 6, 7, 8, 9}),
+			expected:     nil,
+			expectError:  context.Canceled,
+		},
+		{
+			name:         "error case",
+			ctxCancelled: false,
+			inputs:       ptrsArray([]int{1, 2, 3, 4, -5, 6, 7, 8, 9}),
+			expected:     nil,
+			expectError:  errNegativeNumber,
+		},
+		{
+			name:         "one nil input",
+			ctxCancelled: false,
+			inputs:       ptrsArray([]int{1, 0, 3, 4, 5, 6, 7, 8, 9}),
+			expected:     nil,
+			expectError:  nil,
+		},
+		{
+			name:         "all nil inputs",
+			ctxCancelled: false,
+			inputs:       ptrsArray([]int{0, 0, 0, 0, 0, 0, 0, 0, 0}),
+			expected:     nil,
+			expectError:  nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			ctx := t.Context()
+			if tt.ctxCancelled {
+				ctx = cancelledCtx(t)
+			}
+
+			sum, product, minimum, maximum, avg, err := ptr.Apply95CtxErr(
+				ctx,
+				tt.inputs[0], tt.inputs[1], tt.inputs[2],
+				tt.inputs[3], tt.inputs[4], tt.inputs[5],
+				tt.inputs[6], tt.inputs[7], tt.inputs[8],
+				func(ctx context.Context, num1, num2, num3, num4, num5, num6, num7, num8, num9 int,
+				) (sum, product, mn, mx, avg int, err error) {
+					select {
+					default:
+					case <-ctx.Done():
+						return 0, 0, 0, 0, 0, ctx.Err()
+					}
+
+					if min(num1, num2, num3, num4, num5, num6, num7, num8, num9) < 0 {
+						return 0, 0, 0, 0, 0, errNegativeNumber
+					}
+
+					sum = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9
+					product = num1 * num2 * num3 * num4 * num5 * num6 * num7 * num8 * num9
+					mn = min(num1, num2, num3, num4, num5, num6, num7, num8, num9)
+					mx = max(num1, num2, num3, num4, num5, num6, num7, num8, num9)
+					avg = sum / 9
+
+					return
+				})
+
+			if tt.expectError != nil {
+				require.ErrorIs(t, err, tt.expectError)
+			} else {
+				require.NoError(t, err)
+
+				if tt.expected == nil {
+					requireAllNil(t, sum, product, minimum, maximum, avg)
+				} else {
+					requireExpected(t, *tt.expected, sum, product, minimum, maximum, avg)
+				}
 			}
 		})
 	}
