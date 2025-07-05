07d10c276cba62b6a9f80f707f7e81192f008dae
sr9000 <Pirat-spy@yandex.ru>
2025-07-05 21:59:02 +0300

    ft-optional-value: ptr constructors

--- ptr/ptr.go
+++ ptr/ptr.go
@@ -5,3 +5,36 @@ package ptr
 func New[T any](v T) *T {
 	return &v
 }
+
+// FromOk creates a new pointer from a value and an ok flag.
+// If ok equals true, it returns a pointer to val, else nil.
+func FromOk[T any](val T, ok bool) *T {
+	if ok {
+		return &val
+	}
+
+	return nil
+}
+
+// FromErr creates a new pointer from a value and an error.
+//   - If the error is nil, it returns a pointer to val.
+//   - If the error is not nil, it returns nil.
+func FromErr[T any](val T, err error) *T {
+	if err == nil {
+		return &val
+	}
+
+	return nil
+}
+
+// FromZero creates a new pointer from a zeroable (emptiable) value.
+//   - If the value is not equal to its zero value, it returns  a pointer to val.
+//   - If the value is equal to its zero value, it returns nil.
+func FromZero[T comparable](v T) *T {
+	var zero T
+	if v != zero {
+		return &v
+	}
+
+	return nil
+}
